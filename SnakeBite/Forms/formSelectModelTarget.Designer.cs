55f60d676b8080de7180d943ae575b6fb216496f
topher-au <lordramza@gmail.com>
2015-10-31 01:25:02 +1100

    Added gui for model swap (NYI)

---
+++ SnakeBite/Forms/formSelectModelTarget.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SnakeBite
+{
+    partial class formSelectModelTarget
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFormText = new System.Windows.Forms.Label();
+            this.groupModelSelect = new System.Windows.Forms.GroupBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFormText
+            // 
+            this.labelFormText.AutoSize = true;
+            this.labelFormText.Location = new System.Drawing.Point(12, 9);
+            this.labelFormText.Name = "labelFormText";
+            this.labelFormText.Size = new System.Drawing.Size(35, 13);
+            this.labelFormText.TabIndex = 0;
+            this.labelFormText.Text = "label1";
+            // 
+            // groupModelSelect
+            // 
+            this.groupModelSelect.Location = new System.Drawing.Point(15, 25);
+            this.groupModelSelect.Name = "groupModelSelect";
+            this.groupModelSelect.Size = new System.Drawing.Size(257, 92);
+            this.groupModelSelect.TabIndex = 1;
+            this.groupModelSelect.TabStop = false;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(116, 123);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonConfirm.Location = new System.Drawing.Point(197, 123);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfirm.TabIndex = 3;
+            this.buttonConfirm.Text = "Confirm";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
+            // 
+            // formSelectModelTarget
+            // 
+            this.AcceptButton = this.buttonConfirm;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.groupModelSelect);
+            this.Controls.Add(this.labelFormText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formSelectModelTarget";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Confirm Model Replacement";
+            this.Load += new System.EventHandler(this.formSelectModelTarget_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFormText;
+        private System.Windows.Forms.GroupBox groupModelSelect;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonConfirm;
+    }
+}
---
+++ SnakeBite/Forms/formSelectModelTarget.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeBite
+{
+    public partial class formSelectModelTarget : Form
+    {
+        private string formText;
+        private List<string> modelNames;
+        
+        public formSelectModelTarget(string FormText, List<string> ModelNames, int Width=300)
+        {
+            InitializeComponent();
+            formText = FormText;
+            modelNames = ModelNames;
+        }
+
+        private void formSelectModelTarget_Load(object sender, EventArgs e)
+        {
+            // Set form text
+            labelFormText.Text = formText;
+
+            // Create new radio button for each model passed to constructor
+            for (int i = 0; i < modelNames.Count; i++)
+            {
+                groupModelSelect.Controls.Add(new RadioButton() { Text = modelNames[i], Top = i * 20 + 12, Left = 12 , Width = groupModelSelect.Width-24, Tag = i});
+                groupModelSelect.Height = i * 20 + 42;
+            }
+
+            buttonCancel.Top = groupModelSelect.Top + groupModelSelect.Height + 8;
+            buttonConfirm.Top = groupModelSelect.Top + groupModelSelect.Height + 8;
+
+            this.Height = buttonConfirm.Top + buttonConfirm.Height + 48;
+
+            RadioButton r = groupModelSelect.Controls[0] as RadioButton;
+            r.Checked = true;
+            buttonConfirm.Focus();
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            foreach(RadioButton r in groupModelSelect.Controls)
+            {
+                if (r.Checked) this.Tag = r.Tag;
+            }
+        }
+    }
+}
