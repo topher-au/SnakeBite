87d297b4a8410a6456e4a4c4ebf4d27fc74727c0
topher-au <lordramza@gmail.com>
2015-11-10 18:18:31 +1100

    Initial log output

--- SnakeBite/Program.cs
+++ SnakeBite/Program.cs
@@ -12,6 +12,20 @@ namespace SnakeBite
         private static void Main()
         {
             Properties.Settings.Default.Upgrade();
+            Debug.Clear();
+
+            string InitLog = String.Format(
+                "SnakeBite Version: {0}\n" +
+                "-------------------------\n" +
+                "MGS Install Folder: {1}\n" +
+                "MGS Version: {2}\n" +
+                "-------------------------\n",
+                ModManager.GetSBVersion(),
+                Properties.Settings.Default.InstallPath,
+                ModManager.GetMGSVersion());
+
+            Debug.LogLine(InitLog, Debug.LogLevel.Basic);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new formMain());
